5500b708f1ecf201e055b160ecc39addc4beac10
Nikita Viira <aanikita62@gmail.com>
2024-01-21 18:31:12 +0200

    initial commit
    Backend: spring + gradle + postgresql + liquibase
    
    Frontend: vue3 + vite

---
+++ backend/src/test/java/com/rik/programmeerija_prooviylesanne/MainTests.java
@@ -0,0 +1,13 @@
+package com.rik.programmeerija_prooviylesanne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
