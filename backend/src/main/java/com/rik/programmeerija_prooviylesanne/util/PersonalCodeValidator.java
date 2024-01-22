de8d298f5597e5ebb444aa4eb5fe8b578a754c96
Nikita Viira <aanikita62@gmail.com>
2024-01-22 14:01:04 +0200

    add person code validator

---
+++ backend/src/main/java/com/rik/programmeerija_prooviylesanne/util/PersonalCodeValidator.java
@@ -0,0 +1,77 @@
+package com.rik.programmeerija_prooviylesanne.util;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.List;
+
+import static java.lang.Character.getNumericValue;
+import static java.time.format.DateTimeFormatter.ofPattern;
+import static java.util.Arrays.stream;
+import static java.util.Objects.isNull;
+
+public class PersonalCodeValidator {
+    private static final DateTimeFormatter DATE_FORMATTER = ofPattern("uuuuMMdd");
+
+    public static boolean validatePersonalCode(String personalCode) {
+        if (personalCode == null || personalCode.length() != 11) {
+            return false;
+        }
+
+        if (isNull(parseDateOfBirth(personalCode))) {
+            return false;
+        }
+
+        return calculateControlNumber(personalCode) == getNumericValue(personalCode.charAt(personalCode.length() - 1));
+    }
+
+    private static int calculateControlNumber(String personalCode) {
+        String[] numberArray = personalCode.substring(0, 10).split("");
+        List<Integer> numberList = stream(numberArray).map(Integer::valueOf).toList();
+        int sum = 0;
+        int[] multipliers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 1};
+
+        for (int i = 0; i < numberList.size(); i++) {
+            sum += numberList.get(i) * multipliers[i];
+        }
+
+        int parsedControlNumber = sum % 11;
+
+        if (parsedControlNumber == 10) {
+            sum = 0;
+            multipliers = new int[]{3, 4, 5, 6, 7, 8, 9, 1, 2, 3};
+
+            for (int i = 0; i < numberList.size(); i++) {
+                sum += numberList.get(i) * multipliers[i];
+            }
+
+            parsedControlNumber = sum % 11;
+
+            if (parsedControlNumber == 10) {
+                return 0;
+            }
+        }
+
+        return parsedControlNumber;
+    }
+
+    private static LocalDate parseDateOfBirth(String personalCode) {
+        String dateString = personalCode.substring(1, 7);
+
+        dateString = switch (getGenderIdentifier(personalCode)) {
+            case 1, 2 -> "18" + dateString;
+            case 3, 4 -> "19" + dateString;
+            default -> "20" + dateString;
+        };
+
+        try {
+            return LocalDate.parse(dateString, DATE_FORMATTER);
+        } catch (DateTimeParseException e) {
+            return null;
+        }
+    }
+
+    private static int getGenderIdentifier(String personalCode) {
+        return getNumericValue(personalCode.charAt(0));
+    }
+}
---
+++ backend/src/test/java/com/rik/programmeerija_prooviylesanne/PersonalCodeValidatorTest.java
@@ -0,0 +1,45 @@
+package com.rik.programmeerija_prooviylesanne;
+
+import org.junit.jupiter.api.Test;
+
+import static com.rik.programmeerija_prooviylesanne.util.PersonalCodeValidator.validatePersonalCode;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class PersonalCodeValidatorTest {
+    @Test
+    public void test_valid() {
+        String validPersonalCode = "39807093721";
+        assertTrue(validatePersonalCode(validPersonalCode));
+    }
+
+    @Test
+    public void test_tooShort() {
+        String validPersonalCode = "3980709372";
+        assertFalse(validatePersonalCode(validPersonalCode));
+    }
+
+    @Test
+    public void test_invalid() {
+        String invalidPersonalCode = "12345678901";
+        assertFalse(validatePersonalCode(invalidPersonalCode));
+    }
+
+    @Test
+    public void test_invalidFormat() {
+        String invalidFormatPersonalCode = "ABCDEF12345";
+        assertFalse(validatePersonalCode(invalidFormatPersonalCode));
+    }
+
+    @Test
+    public void test_invalidBirthdate() {
+        String invalidBirthdatePersonalCode = "99001310965";
+        assertFalse(validatePersonalCode(invalidBirthdatePersonalCode));
+    }
+
+    @Test
+    public void test_invalidCheckDigit() {
+        String invalidCheckDigitPersonalCode = "49002010964";
+        assertFalse(validatePersonalCode(invalidCheckDigitPersonalCode));
+    }
+}
