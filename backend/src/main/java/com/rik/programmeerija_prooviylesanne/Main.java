f6f97560b3c20674a3ed37b4f5a04348156573e2
Nikita Viira <aanikita62@gmail.com>
2024-01-23 13:16:23 +0200

    add CORS configuration for backend

--- backend/src/main/java/com/rik/programmeerija_prooviylesanne/Main.java
+++ backend/src/main/java/com/rik/programmeerija_prooviylesanne/Main.java
@@ -1,13 +1,31 @@
 package com.rik.programmeerija_prooviylesanne;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Main {
+	@Value("${cors.allowed-origins}")
+	private String[] allowedOrigins;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Main.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry
+						.addMapping("/api/**")
+						.allowedOrigins(allowedOrigins)
+						.allowedMethods("*");
+			}
+		};
+	}
 }
