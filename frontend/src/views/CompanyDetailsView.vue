b78376427617187062b3ab620b9e91571ca41d5e
Nikita Viira <aanikita62@gmail.com>
2024-01-24 15:35:32 +0200

    add company and person details page mockups

---
+++ frontend/src/views/CompanyDetailsView.vue
@@ -0,0 +1,18 @@
+<template>
+  <Form title="Osavõtja info">
+    <div class="form-container p-4">
+      <p class="form-title fw-normal fs-3">
+        Osavõtja info
+      </p>
+      <CompanyForm @back="() => {}" />
+    </div>
+  </Form>
+</template>
+
+<script setup lang="ts">
+  import CompanyForm from '@/components/CompanyForm.vue';
+  import Form from '@/components/Form.vue';
+  import { useRoute } from 'vue-router';
+
+  const route = useRoute();
+</script>
---
+++ frontend/src/views/PersonDetailsView.vue
@@ -0,0 +1,18 @@
+<template>
+  <Form title="Osavõtja info">
+    <div class="form-container p-4">
+      <p class="form-title fw-normal fs-3">
+        Osavõtja info
+      </p>
+      <PersonForm @back="() => {}" />
+    </div>
+  </Form>
+</template>
+
+<script setup lang="ts">
+  import Form from '@/components/Form.vue';
+  import PersonForm from '@/components/PersonForm.vue';
+  import { useRoute } from 'vue-router';
+
+  const route = useRoute();
+</script>
