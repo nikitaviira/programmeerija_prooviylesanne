9805356f9f64de715d6f912911097122d20d6e9b
Nikita Viira <aanikita62@gmail.com>
2024-01-25 14:03:11 +0200

    redirect to 404 page in case of a 404 error from backend

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -2,6 +2,7 @@ import type { AxiosInstance } from 'axios';
 import Axios from 'axios';
 import { toast } from 'vue3-toastify';
 import type { ErrorDto } from '@/api/types';
+import router from '@/router';
 
 export default (): AxiosInstance => {
   const instance = Axios.create({
@@ -11,15 +12,20 @@ export default (): AxiosInstance => {
   instance.defaults.headers.common['Access-Control-Allow-Origin'] = import.meta.env.VITE_API_BASE_URL;
 
   instance.interceptors.response.use((x) => x, async(error) => {
-    const errorData: ErrorDto = error.response.data;
-    if (errorData) {
-      if (errorData.fields.length > 0) {
-        toast(`<ul>${errorData.fields.map((error) => `<li>${error}</li>`).join('')}</ul>`);
-      } else {
-        toast(errorData.message);
+    if (error.response.status === 404) {
+      await router.push('/404');
+    } else {
+      const errorData: ErrorDto = error.response.data;
+      if (error.response.data) {
+        if (errorData.fields.length > 0) {
+          toast(`<ul>${errorData.fields.map((error) => `<li>${error}</li>`).join('')}</ul>`);
+        } else {
+          toast(errorData.message);
+        }
       }
+
+      throw error;
     }
-    throw error;
   });
 
   return instance;
