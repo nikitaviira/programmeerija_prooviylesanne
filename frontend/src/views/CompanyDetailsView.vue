9880ad26bd745cf68cd4664080bf8e290e20340b
Nikita Viira <aanikita62@gmail.com>
2024-01-25 14:10:06 +0200

    load actual data in details view, update on form save

--- frontend/src/views/CompanyDetailsView.vue
+++ frontend/src/views/CompanyDetailsView.vue
@@ -4,7 +4,11 @@
       <p class="form-title fw-normal fs-3">
         Osavõtja info
       </p>
-      <CompanyForm @back="() => {}" />
+      <CompanyForm
+        :company="company"
+        @saved="updateCompany"
+        @back="toHomePage"
+      />
     </div>
   </Form>
 </template>
@@ -12,7 +16,26 @@
 <script setup lang="ts">
   import CompanyForm from '@/components/CompanyForm.vue';
   import Form from '@/components/Form.vue';
-  import { useRoute } from 'vue-router';
+  import { useRouter } from 'vue-router';
+  import { onBeforeMount, ref } from 'vue';
+  import CompanyApi from '@/api/controllers/company';
+  import type { CompanyDto } from '@/api/types';
 
-  const route = useRoute();
+  const router = useRouter();
+  const companyId: number = parseInt(router.currentRoute.value.params.id as string);
+  const company = ref<CompanyDto>();
+
+  onBeforeMount(async() => {
+    const { data } = await CompanyApi.company(companyId);
+    company.value = data;
+  });
+
+  async function updateCompany(company: CompanyDto) {
+    await CompanyApi.updateCompany(companyId, company);
+    await toHomePage();
+  }
+
+  async function toHomePage() {
+    await router.push('/');
+  }
 </script>
--- frontend/src/views/PersonDetailsView.vue
+++ frontend/src/views/PersonDetailsView.vue
@@ -4,15 +4,44 @@
       <p class="form-title fw-normal fs-3">
         Osavõtja info
       </p>
-      <PersonForm @back="() => {}" />
+      <PersonForm
+        :person="person"
+        @saved="updatePerson"
+        @back="toHomePage"
+      />
     </div>
   </Form>
 </template>
 
 <script setup lang="ts">
   import Form from '@/components/Form.vue';
+  import { useRouter } from 'vue-router';
+  import { onBeforeMount, ref } from 'vue';
+  import PersonApi from '@/api/controllers/person';
+  import { type PersonDto } from '@/api/types';
   import PersonForm from '@/components/PersonForm.vue';
-  import { useRoute } from 'vue-router';
 
-  const route = useRoute();
+  const router = useRouter();
+  const personId: number = parseInt(router.currentRoute.value.params.id as string);
+  const person = ref<PersonDto>({
+    firstName: '',
+    lastName: '',
+    personalCode: '',
+    paymentType: undefined,
+    info: ''
+  });
+
+  onBeforeMount(async() => {
+    const { data } = await PersonApi.person(personId);
+    person.value = data;
+  });
+
+  async function updatePerson(person: PersonDto) {
+    await PersonApi.updatePerson(personId, person);
+    await toHomePage();
+  }
+
+  async function toHomePage() {
+    await router.push('/');
+  }
 </script>
